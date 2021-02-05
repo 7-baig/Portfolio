cea708995aef477a41f4478a2e1c997cf5cb6f5f
7-baig <saadbaig7777@gmail.com>
2021-02-05 13:33:55 +0500

    created header

---
+++ src/components/Layout/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import styles from "./Header.module.scss"
+
+const Header = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.leftContainer}>
+        <div className={styles.nameContainer}>
+          <span className={styles.circle}>S</span>
+          <p>
+            <span>Saad</span> Baig
+          </p>
+        </div>
+      </div>
+      <div className={styles.rightContainer}>
+        <ul>
+          <li>Skills</li>
+          <li>Work Experiences</li>
+          <li>Projects</li>
+          <li>Blog</li>
+          <li>Contact Me</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Header
