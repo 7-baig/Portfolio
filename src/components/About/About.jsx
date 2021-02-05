9d2ee84af70a77fc6c45967c337ab1ea1b1f5ddc
7-baig <saadbaig7777@gmail.com>
2021-02-05 20:16:26 +0500

    updated about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,10 @@
 import React from "react"
 import vector from "../../images/about.svg"
+import desktop from "../../images/icons/desktop.png"
+import plane from "../../images/icons/plane.png"
+import flag from "../../images/icons/pakistan-flag.png"
+import write from "../../images/icons/write.png"
+import books from "../../images/icons/books.png"
 import styles from "./About.module.scss"
 
 const About = () => {
@@ -12,9 +17,14 @@ const About = () => {
         <p>Full-Stack Developer</p>
         <h1>Saad Baig</h1>
         <p>
-          Hi, I'm a Full-Stack Web developer, working with web technologies for
-          2+ years. Besides Coding, I like to read books, write articles and
-          travel
+          Hi, I'm a Full-Stack Web developer from Pakistan{" "}
+          <img className={styles.icon} src={flag} alt="pakistan-flag" />,
+          working with web technologies{" "}
+          <img className={styles.icon} src={desktop} alt="desktop-icon" /> for
+          2+ years. Besides Coding, I like to read books{" "}
+          <img className={styles.icon} src={books} alt="books" />, write
+          articles <img className={styles.icon} src={write} alt="writing" /> and
+          travel <img className={styles.icon} src={plane} alt="plane" />.
         </p>
         <div className={styles.btnContainer}>
           <button>Download CV</button>
--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import styles from './Footer.module.scss'
+import React from "react"
+import styles from "./Footer.module.scss"
 
 const Footer = () => {
-    return (
-        <div>
-            footer
-        </div>
-    )
+  return (
+    <footer className={styles.container}>
+      <p>Made with <i className="fas fa-heart"></i> by Saad Baig</p>
+    </footer>
+  )
 }
 
 export default Footer
