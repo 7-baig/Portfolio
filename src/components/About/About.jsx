d2ca1b27bf30fa375551d6005d845927e0d94fd5
7-baig <saadbaig7777@gmail.com>
2021-02-06 13:14:36 +0500

    updated about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,6 +5,9 @@ import plane from "../../images/icons/plane.png"
 import flag from "../../images/icons/pakistan-flag.png"
 import write from "../../images/icons/write.png"
 import books from "../../images/icons/books.png"
+import laptop from "../../images/icons/laptop.png"
+import drawing from "../../images/icons/drawing.png"
+import twitter from "../../images/icons/twitter.png"
 import styles from "./About.module.scss"
 
 const About = () => {
@@ -21,10 +24,15 @@ const About = () => {
           <img className={styles.icon} src={flag} alt="pakistan-flag" />,
           working with web technologies{" "}
           <img className={styles.icon} src={desktop} alt="desktop-icon" /> for
-          2+ years. Besides Coding, I like to read books{" "}
-          <img className={styles.icon} src={books} alt="books" />, write
-          articles <img className={styles.icon} src={write} alt="writing" /> and
-          travel <img className={styles.icon} src={plane} alt="plane" />.
+          2+ years. When I'm not Coding{" "}
+          <img className={styles.icon} src={laptop} alt="laptop" />, writing
+          articles <img className={styles.icon} src={write} alt="writing" /> or
+          tweeting{" "}
+          <img className={styles.icon} src={twitter} alt="twitter-icon" />,
+          you'll find me reading books{" "}
+          <img className={styles.icon} src={books} alt="books" />, drawing{" "}
+          <img className={styles.icon} src={drawing} alt="drawing-icon" /> and
+          travelling <img className={styles.icon} src={plane} alt="plane" />.
         </p>
         <div className={styles.btnContainer}>
           <button>Download CV</button>
