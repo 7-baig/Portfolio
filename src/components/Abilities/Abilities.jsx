52beea13535096621ee3c0cac5784c607586d8b3
7-baig <saadbaig7777@gmail.com>
2021-02-06 21:48:54 +0500

    added what i do section

--- src/components/Abilities/Abilities.jsx
+++ src/components/Abilities/Abilities.jsx
@@ -2,16 +2,18 @@ import React from "react"
 import Quote from "../Quote/Quote"
 import styles from "./Abilities.module.scss"
 import Skills from "./Skills/Skills"
+import WhatIDo from "./WhatIDo/WhatIDo"
 
 const Abilities = () => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} id="skills">
       <h1>Abilities</h1>
       <Quote
         quote="Life without knowledge is death in disguise."
         author="Talib Kweli"
       />
       <Skills />
+      <WhatIDo />
     </div>
   )
 }
---
+++ src/components/Abilities/WhatIDo/WhatIDo.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import styles from "./WhatIDo.module.scss"
+
+const WhatIDo = () => {
+  return (
+    <div className={styles.container}>
+      <h2>What I Do?</h2>
+      <div className={styles.content}>
+        <div className={styles.row}>
+          <i className={styles.icon + " fas fa-bolt"}></i>
+          <p>Focus on writing clean, elegant and efficient code.</p>
+        </div>
+        <div className={styles.row}>
+          <i className={styles.icon + " fas fa-bolt"}></i>
+          <p>Develop responsive and highly interactive Front End / User Interfaces.</p>
+        </div>
+        <div className={styles.row}>
+          <i className={styles.icon + " fas fa-bolt"}></i>
+          <p>Integrate NodeJS APIs in React.</p>
+        </div>
+        <div className={styles.row}>
+          <i className={styles.icon + " fas fa-bolt"}></i>
+          <p>Transform Design into React High Quality Code.</p>
+        </div>
+        <div className={styles.row}>
+          <i className={styles.icon + " fas fa-bolt"}></i>
+          <p>Create Backend Applications in Node/Express.</p>
+        </div>
+        <div className={styles.row}>
+          <i className={styles.icon + " fas fa-bolt"}></i>
+          <p>Eat Pizza.</p>
+        </div>
+        <div className={styles.row}>
+          <i className={styles.icon + " fas fa-bolt"}></i>
+          <p>Swear at computer.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WhatIDo
