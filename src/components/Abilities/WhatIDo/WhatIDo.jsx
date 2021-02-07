7c6ab18b02136d203cd4e43cfc94edcc5257bb00
7-baig <saadbaig7777@gmail.com>
2021-02-07 16:59:19 +0500

    updated whatIDo Section

--- src/components/Abilities/WhatIDo/WhatIDo.jsx
+++ src/components/Abilities/WhatIDo/WhatIDo.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import comet from '../../../images/facts/comet.png'
 import styles from "./WhatIDo.module.scss"
 
 const WhatIDo = () => {
@@ -7,32 +8,32 @@ const WhatIDo = () => {
       <h2>What I Do?</h2>
       <div className={styles.content}>
         <div className={styles.row}>
-          <i className={styles.icon + " fas fa-bolt"}></i>
-          <p>Focus on writing clean, elegant and efficient code.</p>
-        </div>
-        <div className={styles.row}>
-          <i className={styles.icon + " fas fa-bolt"}></i>
+          <img src={comet} alt="comet" />
           <p>Develop responsive and highly interactive Front End / User Interfaces.</p>
         </div>
         <div className={styles.row}>
-          <i className={styles.icon + " fas fa-bolt"}></i>
-          <p>Integrate NodeJS APIs in React.</p>
+          <img src={comet} alt="comet" />
+          <p>Focus on writing clean, elegant and efficient code.</p>
         </div>
         <div className={styles.row}>
-          <i className={styles.icon + " fas fa-bolt"}></i>
-          <p>Transform Design into High Quality Code.</p>
-        </div>
-        <div className={styles.row}>
-          <i className={styles.icon + " fas fa-bolt"}></i>
+          <img src={comet} alt="comet" />
           <p>Create Backend Applications in Node/Express.</p>
         </div>
         <div className={styles.row}>
-          <i className={styles.icon + " fas fa-bolt"}></i>
-          <p>Eat Pizza.</p>
+          <img src={comet} alt="comet" />
+          <p>Transform Design into High Quality Code.</p>
         </div>
         <div className={styles.row}>
-          <i className={styles.icon + " fas fa-bolt"}></i>
+          <img src={comet} alt="comet" />
+          <p>Integrate NodeJS APIs in React.</p>
+        </div>
+        <div className={styles.row}>
+          <img src={comet} alt="comet" />
           <p>Swear at computer.</p>
+        </div>
+        <div className={styles.row}>
+          <img src={comet} alt="comet" />
+          <p>Eat Pizza.</p>
         </div>
       </div>
     </div>
