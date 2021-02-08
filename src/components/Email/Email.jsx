e2fd68045ad250f61d68d1e42c539c7d4f312faf
7-baig <saadbaig7777@gmail.com>
2021-02-08 16:34:26 +0500

    mailto setup

--- src/components/Email/Email.jsx
+++ src/components/Email/Email.jsx
@@ -1,7 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "./Email.module.scss"
 
 const Email = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState("")
   return (
     <div className={styles.container}>
       <h1>Send me an email</h1>
@@ -10,21 +13,43 @@ const Email = () => {
           <div className={styles.leftContainer}>
             <div className={styles.field}>
               <label>Your name:</label>
-              <input type="text"></input>
+              <input
+                type="text"
+                value={name}
+                name="name"
+                required
+                onChange={e => setName(e.target.value)}
+              ></input>
             </div>
             <div className={styles.field}>
               <label>Your email:</label>
-              <input type="text"></input>
+              <input
+                type="text"
+                value={email}
+                name="email"
+                required
+                onChange={e => setEmail(e.target.value)}
+              ></input>
             </div>
           </div>
           <div className={styles.rightContainer}>
             <div className={styles.field}>
               <label>Your message:</label>
-              <textarea type="text" />
+              <textarea
+                type="text"
+                value={message}
+                name="message"
+                required
+                onChange={e => setMessage(e.target.value)}
+              />
             </div>
           </div>
         </div>
-        <button>Send email</button>
+        <a
+          href={`mailto:saadbaig7777@gmail.com?subject=${name}%20contacting%20Saad%20Baig&body=${message}&from=hello`}
+        >
+          Send email
+        </a>
       </form>
     </div>
   )
