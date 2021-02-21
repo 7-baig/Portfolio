df0bb6790164700d3981774f310c805c1d7515ff
7-baig <saadbaig7777@gmail.com>
2021-02-21 15:13:19 +0500

    added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,16 @@ module.exports = {
       },
     },
     `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "UA-126452483-2", // Google Analytics / GA
+          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+      },
+    }
   ],
   pathPrefix: "/portfolio",
 }
