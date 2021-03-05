241a5a420fdfefd3ed4c0498609b2af8be1b3a11
7-baig <saadbaig7777@gmail.com>
2021-03-05 20:20:54 +0500

    updated blog section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -28,33 +28,17 @@ const Projects = () => {
             <img src={kpibar} alt="kpibar" loading="lazy" />
             <h3 className={styles.show}>KPIBAR's Website</h3>
             <p>React - Gatsby - CSS3 - Firebase</p>
-            <button>
+            <button className={styles.viewSite}>
               <a href="https://website-pre.kpibar.com/" target="_blank">
                 View site
               </a>
             </button>
           </div>
-          {/* <div className={styles.project}>
-            <img src={sample} alt="sample" loading="lazy" />
-            <h3 className={styles.show}>E-Commerce Application</h3>
-            <p>React/Redux - Node - MongoDB - Express</p>
-            <button>
-              <a
-                href="https://github.com/7-baig/MERN_E-COMMERCE_APP"
-                target="_blank"
-              >
-                Github Repo
-              </a>
-            </button>
-            <button onClick={() => {
-              setIsOpenModal(!isOpenModal)
-            }}>Watch Video</button>
-          </div> */}
           <div className={styles.project}>
             <img src={rdi} alt="sample" loading="lazy" />
             <h3 className={styles.show}>RDI Solutions</h3>
             <p>ReactJS - Ant design</p>
-            <button>
+            <button className={styles.viewSite}>
               <a href="https://rdi-solutions.netlify.app/" target="_blank">
                 View site
               </a>
@@ -65,7 +49,7 @@ const Projects = () => {
             <img src={cart} alt="cart" loading="lazy" />
             <h3 className={styles.show}>On-Cart CMS</h3>
             <p>React/Redux - Bootstrap</p>
-            <button>
+            <button className={styles.viewSite}>
               <a href="https://oncartadmin.web.app/" target="_blank">
                 View site
               </a>
@@ -75,7 +59,7 @@ const Projects = () => {
             <img src={limo} alt="limo" loading="lazy" />
             <h3 className={styles.show}>Admirals CMS</h3>
             <p>React-Redux - Bootstrap - Node</p>
-            <button>
+            <button className={styles.viewSite}>
               <a href="https://admiral-cms.netlify.app/" target="_blank">
                 View site
               </a>
@@ -85,12 +69,12 @@ const Projects = () => {
             <img src={quiz} alt="quiz" loading="lazy" />
             <h3 className={styles.show}>Quiz App (PWA)</h3>
             <p>React JS - TypeScript - PWA</p>
-            <button>
+            <button className={styles.viewSite}>
               <a href="https://quiz-app-bc10d.web.app/" target="_blank">
                 View site
               </a>
             </button>
-            <button>
+            <button className={styles.gitBtn}>
               <a href="https://github.com/7-baig/Quiz-PWA" target="_blank">
                 Github Repo
               </a>
@@ -100,7 +84,7 @@ const Projects = () => {
             <img src={football} alt="football" loading="lazy" />
             <h3 className={styles.show}>Covid-19 tracker App</h3>
             <p>React - Charts JS - Material UI</p>
-            <button>
+            <button className={styles.viewSite}>
               <a
                 href="http://covid-19-tracker-app-saad-baig.surge.sh/"
                 target="_blank"
@@ -108,7 +92,7 @@ const Projects = () => {
                 View site
               </a>
             </button>
-            <button>
+            <button className={styles.gitBtn}>
               <a
                 href="https://github.com/saadbaigg/Covid-19-Tracker-App"
                 target="_blank"
